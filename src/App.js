55236fde573c113278fafb33b129cae8142f06c0
Nishant Maurya <nish2493@rocketmail.com>
2022-01-25 15:26:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   return (
     <>
       <Appbar />
-      <Grid container spacing={3}>
-        <Grid item xs={3}></Grid>
-        <Grid item xs={6}>
+      <Grid container spacing={3} sx={{ mt: 1 }}>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={8}>
           <Home />
         </Grid>
-        <Grid item xs={3}></Grid>
+        <Grid item xs={2}></Grid>
       </Grid>
     </>
   );
