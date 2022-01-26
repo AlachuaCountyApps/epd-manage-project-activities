67858e9dc28f18bfb08371d7c3d9aa84b3cd07bb
Nishant Maurya <nish2493@rocketmail.com>
2022-01-25 21:49:19 -0500

    program dropdowns

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { Grid } from '@mui/material';
-import Appbar from './components/Appbar';
+import { useState } from 'react';
 
+import Appbar from './components/Appbar';
 import Home from './components/Home';
 
 function App() {
+  const [program, setProgram] = useState('');
+
+  const handleProgramChange = (event) => {
+    setProgram(event.target.value);
+  };
+
   return (
     <>
       <Appbar />
       <Grid container spacing={3} sx={{ mt: 1 }}>
         <Grid item xs={2}></Grid>
         <Grid item xs={8}>
-          <Home />
+          <Home program={program} handleProgramChange={handleProgramChange} />
         </Grid>
         <Grid item xs={2}></Grid>
       </Grid>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
-import { Grid } from '@mui/material';
-import React from 'react';
+import { FormControl, Grid, InputLabel, MenuItem, Select } from '@mui/material';
 
-export default function Home() {
+export default function Home({ program, handleProgramChange }) {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-        Test
+        <FormControl fullWidth>
+          <InputLabel id='select-program'>Select Program</InputLabel>
+          <Select
+            labelId='select-program'
+            id='program'
+            value={program}
+            label='Select Program'
+            onChange={handleProgramChange}
+          >
+            <MenuItem value={1668241403}>Admin</MenuItem>
+            <MenuItem value={928287777}>Hazmat</MenuItem>
+            <MenuItem value={925232059}>Land</MenuItem>
+            <MenuItem value={1503973566}>Natural Resources</MenuItem>
+            <MenuItem value={1502946810}>Petroleum</MenuItem>
+            <MenuItem value={925231091}>Water</MenuItem>
+          </Select>
+        </FormControl>
       </Grid>
     </Grid>
   );
